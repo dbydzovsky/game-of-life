fd7f15ac22f91493fee69556493239c45980b111
d.bydzovsky <d.bydzovsky@quadient.com>
2024-11-19 12:22:14 +0000

    correct package

---
+++ src/test/java/cz/bydzovsky/internship/gameoflife/model/WorldImplTest.java
@@ -0,0 +1,14 @@
+package cz.bydzovsky.internship.gameoflife.model;
+
+import org.junit.jupiter.api.Test;
+
+/*
+ * Author : Bydzovsky Dominik
+ * Date Created: 2024/11/19
+ */
+class WorldImplTest {
+    @Test
+    public void test() {
+        WorldImpl world = new WorldImpl(10, 10);
+    }
+}
